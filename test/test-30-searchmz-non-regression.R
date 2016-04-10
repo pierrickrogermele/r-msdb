58800199613c5fcd12a852a901e97bbfcb78442c
Pierrick Roger Mele <pierrick.roger@gmail.com>
2016-04-10 20:48:43 +0200

    Test W4M ticket 2016031010000034.

--- test/test-30-searchmz-non-regression.R
+++ test/test-30-searchmz-non-regression.R
@@ -4,3 +4,7 @@ test.match.mz.no.y.col.in.output <- function() {
 	df <- read.table('mz-output.tsv', header = TRUE)
 	checkTrue( ! 'y' %in% colnames(df))
 }
+
+test.searchmz.ticket.2016031010000034 <- function() {
+	call.search.mz(c('-d', 'file', '--url', 'res/ticket-2016031010000034-database_CEA_test_2_utf8.tsv', '-m', 'pos', '-i', 'res/ticket-2016031010000034-input_file_for_db_test_2.tsv', '--input-col-names', 'mz=mzmed,rt=rtmed', '-o', 'ticket-2016031010000034-output.tsv', '--peak-output-file', 'ticket-2016031010000034-output-peaks.tsv', '--html-output-file', 'ticket-2016031010000034-output.html'), use.global.conn.flags = FALSE)
+}
