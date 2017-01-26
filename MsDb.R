2d38a35cc1043f40854c3908427a0baef1245b73
Pierrick Roger Mele <pierrick.roger@gmail.com>
2017-01-26 21:18:35 +0100

    Correct names of constants.

--- MsDb.R
+++ MsDb.R
@@ -244,7 +244,7 @@ if ( ! exists('MsDb')) { # Do not load again if already loaded
 	# rt.tol.x          Tolerance parameter for the equations : rtinf = rt - rt.tol.x - rt ^ rt.tol.y and rtsup = rt + rt.tol.x + rt ^ rt.tol.y
 	# rt.tol.y          Tolerance parameter. See rt.tol.x parameter.
 	# attribs           Only search for peaks whose attribution is among this set of attributions.
-	# molids            Only search for peaks whose molecule ID is among this vector of integer molecule IDs. Can also be a data frame with a retention time column x.colnames$rt and a molecule ID column MSDB.TAG.molid.
+	# molids            Only search for peaks whose molecule ID is among this vector of integer molecule IDs. Can also be a data frame with a retention time column x.colnames$rt and a molecule ID column MSDB.TAG.MOLID.
 	# molids.rt.tol     Retention time tolerance used when molids parameter is a data frame (rt, id)
 	# precursor.match   Remove peaks whose molecule precursor peak has not also been matched.
 	# precursor.rt.tol
@@ -379,7 +379,7 @@ if ( ! exists('MsDb')) { # Do not load again if already loaded
 #				else {
 #					if (same.rows) {
 #						y[r, colnames(x.lines)] <- x.lines
-#						ids <- results[[MSDB.TAG.molid]]
+#						ids <- results[[MSDB.TAG.MOLID]]
 #						ids <- ids[ ! duplicated(ids)] # Remove duplicated values
 #						y[r, MSDB.TAG.msmatching] <- paste(ids, collapse = .self$.molids.sep)
 #					}
@@ -436,7 +436,7 @@ if ( ! exists('MsDb')) { # Do not load again if already loaded
 
 		# List molecule IDs
 		if ( ! is.null(molids.rt.tol) && is.data.frame(molids)) {
-			ids <- molids[(rt >= molids[[MSDB.TAG.colrt]] - molids.rt.tol) & (rt <= molids[[MSDB.TAG.colrt]] + molids.rt.tol), MSDB.TAG.molid]
+			ids <- molids[(rt >= molids[[MSDB.TAG.COLRT]] - molids.rt.tol) & (rt <= molids[[MSDB.TAG.COLRT]] + molids.rt.tol), MSDB.TAG.MOLID]
 			if (length(ids) == 0)
 				# No molecule ID match for this retention time
 				return(data.frame()) # return empty result set
