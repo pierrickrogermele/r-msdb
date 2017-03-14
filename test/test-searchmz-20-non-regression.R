4a25cf2b085724f6fa503c247068942c6db508e8
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2017-03-14 15:17:06 +0100

    Write test for Estelle Peakforest issue 20170314.

--- test/test-searchmz-20-non-regression.R
+++ test/test-searchmz-20-non-regression.R
@@ -1,3 +1,7 @@
 test.searchmz.ticket.2016031010000034 <- function() {
 	call.search.mz(c('-d', 'file', '--url', 'res/ticket-2016031010000034-database_CEA_test_2_utf8.tsv', '-m', 'pos', '-i', 'res/ticket-2016031010000034-input_file_for_db_test_2.tsv', '--input-col-names', 'mz=mzmed,rt=rtmed', '-o', 'ticket-2016031010000034-output.tsv', '--peak-output-file', 'ticket-2016031010000034-output-peaks.tsv', '--html-output-file', 'ticket-2016031010000034-output.html'), use.global.conn.flags = FALSE)
 }
+
+test.searchmz.peakforest.estelle.20170314 <- function() {
+	call.search.mz(c('-d', 'peakforest', '--url', 'https://peakforest-alpha.inra.fr/rest', '--db-token', '66jo50hkvi7gbje5m2494fr44q', '-m', 'pos', '-p', '5', '-s', '0', '-i', 'res/peakforest.estelle.20170314-input.tsv', '--input-col-names', 'mz=mzmed,rt=rtmed', '-o', 'peakforest.estelle.20170314-output.tsv'), use.global.conn.flags = FALSE)
+}
