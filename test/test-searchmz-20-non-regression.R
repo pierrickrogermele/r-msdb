d62e5940690072de5c6c4a568e6484899ccdd4d8
Pierrick Roger Mele <pierrick.rogermele@icloud.com>
2017-03-16 09:19:22 +0100

    Add tests for peakforest matching with RT in min and sec.

--- test/test-searchmz-20-non-regression.R
+++ test/test-searchmz-20-non-regression.R
@@ -5,3 +5,15 @@ test.searchmz.ticket.2016031010000034 <- function() {
 test.searchmz.peakforest.estelle.20170314 <- function() {
 	call.search.mz(c('-d', 'peakforest', '--url', 'https://peakforest-alpha.inra.fr/rest', '--db-token', ENV[['RMSBD_PEAKFOREST_TOKEN']], '-m', 'pos', '-p', '5', '-s', '0', '-i', 'res/peakforest.estelle.20170314-input.tsv', '--input-col-names', 'mz=mzmed,rt=rtmed', '-o', 'peakforest.estelle.20170314-output.tsv', '--peak-output-file', 'peakforest.estelle.20170314-output-peaks.tsv', '--html-output-file', 'peakforest.estelle.20170314-output.html'), use.global.conn.flags = FALSE)
 }
+
+test.searchmz.peakforest.estelle.20170316.rtunit.min <- function() {
+	call.search.mz(c('-d', 'peakforest', '--url' ,'https://peakforest-alpha.inra.fr/rest', '--db-token', ENV[['RMSBD_PEAKFOREST_TOKEN']], '-m', 'pos', '-p', '5', '-s', '0', '-i', 'res/2017-03-16-estelle-rtunit.min/input.tsv', '--input-col-names', 'mz=mzmed,rt=rtmed', '-o', 'peakforest.estelle.20170316.rtunit.min-output.tsv', '--peak-output-file', 'peakforest.estelle.20170316.rtunit.min-output-peaks.tsv', '--html-output-file', 'peakforest.estelle.20170316.rtunit.min-output.html', '--rtunit', 'min', '--all-cols', '--rttolx', '5', '--rttoly', '0.8'), use.global.conn.flags = FALSE)
+
+	# Check that output file contains at least one match
+}
+
+test.searchmz.peakforest.estelle.20170316.rtunit.sec <- function() {
+	call.search.mz(c('-d', 'peakforest', '--url' ,'https://peakforest-alpha.inra.fr/rest', '--db-token', ENV[['RMSBD_PEAKFOREST_TOKEN']], '-m', 'pos', '-p', '5', '-s', '0', '-i', 'res/2017-03-16-estelle-rtunit.sec/input.tsv', '--input-col-names', 'mz=mzmed,rt=rtmed', '-o', 'peakforest.estelle.20170316.rtunit.sec-output.tsv', '--peak-output-file', 'peakforest.estelle.20170316.rtunit.sec-output-peaks.tsv', '--html-output-file', 'peakforest.estelle.20170316.rtunit.sec-output.html', '--all-cols', '--rttolx', '5', '--rttoly', '0.8'), use.global.conn.flags = FALSE)
+
+	# Check that output file contains at least one match
+}
